23e1f9a2e95c9571e007a2c4588f93491d028791
mahnafdev <mahnafdev@gmail.com>
2026-03-17 23:41:52 +0600

    chore: initialize project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router";
+import { routes } from "./routes/routes.jsx";
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
+	<StrictMode>
+		<RouterProvider router={routes}></RouterProvider>
+	</StrictMode>,
+);
